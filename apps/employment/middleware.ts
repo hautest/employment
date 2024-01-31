387ae600e49a40895a9b594fd6e9ce22105accce
LeeSeungHun <dos07008@naver.com>
2024-01-31 21:28:10 +0900

    fix: 반응형 새로고침시 바로 대응 안되는 버그 수정

--- apps/employment/middleware.ts
+++ apps/employment/middleware.ts
@@ -1,4 +1,19 @@
+import { RequestCookies, ResponseCookies } from 'next/dist/compiled/@edge-runtime/cookies';
 import { NextRequest, NextResponse, userAgent } from 'next/server';
+
+function applySetCookie(req: NextRequest, res: NextResponse): void {
+  const setCookies = new ResponseCookies(res.headers);
+  const newReqHeaders = new Headers(req.headers);
+  const newReqCookies = new RequestCookies(newReqHeaders);
+  setCookies.getAll().forEach((cookie) => newReqCookies.set(cookie));
+  NextResponse.next({
+    request: { headers: newReqHeaders },
+  }).headers.forEach((value, key) => {
+    if (key === 'x-middleware-override-headers' || key.startsWith('x-middleware-request-')) {
+      res.headers.set(key, value);
+    }
+  });
+}
 
 export function middleware(request: NextRequest) {
   const { device } = userAgent(request);
@@ -6,6 +21,8 @@ export function middleware(request: NextRequest) {
 
   const response = NextResponse.next();
   response.cookies.set('viewport', viewport);
+
+  applySetCookie(request, response);
 
   return response;
 }
